85b7c27baec7de0e00e5d23c733ed7fd1a9d2680
23840932 <74847847+938454098530486308430358043949396037284@users.noreply.github.com>
2022-03-05 17:13:03 -0600

    d
    d

---
+++ menu.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<title>Navigation</title>
+		<link rel='stylesheet' href='/css/font.css' />
+		<link rel='stylesheet' href='/css/main.css' />
+		<link rel='icon' type='image/x-icon' href='/img/favicon.ico' />
+		<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+	</head>
+	<body>
+
+		<div class='nav'>
+			<a href='/'><img src='/img/head.gif' class='navimg' alt='Return' /></a>
+
+			<ul class='nomargin'>
+				<li><a href='/about/'>/about/</a></li>
+				<li><a href='/blog/'>/blog/</a></li>
+				<li><a href='/contact/'>/contact/</a></li>
+				<li><a href='/links/'>/links/</a></li>
+				<li><a href='/other/'>/other/</a></li>
+			</ul>
+
+			<br />
+			<ul class='nomargin'>
+				<li><a href='https://git.concealed.world' target='_blank'>Git</a></li>
+				<li><a href='https://irc.concealed.world' target='_blank'>IRC</a></li>
+			</ul>
+
+			<br /><p class='nomargin'><a href='http://kpzscgdqezpen5zpnc4gky5vx4r56thl5syq2x45j6hbjy5w7tjilwid.onion/menu.php' target='_blank'>[ Use Tor ]</a></p>
+		</div>
+	</body>
+</html>
